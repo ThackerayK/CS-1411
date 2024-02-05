4d30a52fc0e96b2d4dc5e5b8f3711b8a6b780179
Kelly Thackeray <113401678+ThackerayK@users.noreply.github.com>
2024-02-05 11:33:54 -0600

    Add files via upload
    Chapter 3 Exercise 4

---
+++ PE4Chapter3.py
@@ -0,0 +1,28 @@
+#Kelly Thackeray
+#Feb. 5,2024
+#Display Roman Numerals for 1 to 10
+
+number = int(input("Enter an interger from 1 to 10: "))
+
+if number == 1:
+    print("I")
+elif number == 2:
+    print("II")
+elif number == 3:
+    print("III")
+elif number == 4:
+    print("IV")
+elif number == 5:
+    print("V")
+elif number == 6:
+    print("VI")
+elif number == 7 :
+    print("VII")
+elif number == 8:
+    print("VIII")
+elif number == 9:
+    print("IX")
+elif number == 10:
+    print("X")
+else:
+    print("Invalid input. Please check and re-enter.")
